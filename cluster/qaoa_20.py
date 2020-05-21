a16f06e675dc14cabb5c12961250e2ce563fb8f9
Matija Medvidović <matija.medvidovic@gmail.com>
2020-05-21 20:27:53 +0200

    Changes to run pi/8 on the cluster

--- cluster/qaoa_20.py
+++ cluster/qaoa_20.py
@@ -17,10 +17,9 @@ nq = 20
 
 gamma_0, beta_0 = 1.3143397836447093, -0.4327704165659409 # p=1 optimal parameters
 
-beta_1 = np.pi/4 # fixing \beta_1
+beta_1 = np.pi/8 # fixing \beta_1
 
-gammas = np.linspace(0, np.pi/4, size)
-gamma_1 = gammas[r]
+gamma_1 = np.linspace(0, np.pi/4, size)[r]
 
 logpsi = RBM(n_visible=20)
 loaded = logpsi.load('rbm_params_20_qubit_optimal.npz')
@@ -30,7 +29,7 @@ G = nx.from_numpy_matrix(loaded['graph'])
 for i, j in G.edges(): 
     logpsi.RZZ(i, j, phi=2*gamma_1)
 
-logpsi.mask[:] = True
+# logpsi.mask[:] = True
 
 data = {'gamma_0': gamma_0, 'beta_0': beta_0, 'gamma_1': gamma_1, 'beta_1': beta_1}
 key_template = 'proc_{}#after_q{}#{}'
@@ -43,7 +42,7 @@ for n in range(nq):
     print('Qubit {} starting on process {}...'.format(n+1, r))
         
     params, Fs = rx_optimization(logpsi, n, beta_1, tol=tol, lr=lr, lookback=10, resample_phi=1, sigma=0.0,
-                                   psi_mcmc_params=(2000,3,200,30), phi_mcmc_params=(2000,3,200,30),
+                                   psi_mcmc_params=(2000,3,100,15), phi_mcmc_params=(2000,3,100,15),
                                    eps=1e-5, verbose=False)
     
     logpsi.set_flat_params(params)
@@ -59,7 +58,7 @@ for n in range(nq):
 
 #### WRITING FILES ####
 
-save_folder = os.path.join(os.getcwd(), 'output_data')
+save_folder = os.path.join(os.getcwd(), 'output_data_pi8')
 
 if not os.path.exists(save_folder):
     os.mkdir(save_folder)
