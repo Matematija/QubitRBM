59c70f97e2f56328cfff162ac245b892bd446070
Matija Medvidović <matija.medvidovic@gmail.com>
2020-05-31 00:31:18 +0200

    Another run

--- cluster/qaoa_20.py
+++ cluster/qaoa_20.py
@@ -28,37 +28,37 @@ G = nx.from_numpy_matrix(loaded['graph'])
 for i, j in G.edges(): 
     logpsi.RZZ(i, j, phi=2*gamma_1)
 
+logpsi.add_hidden_units(num=4*logpsi.nv - logpsi.nh)
 logpsi.mask[:] = True
-# logpsi.add_hidden_units(num=6*logpsi.nv - logpsi.nh)
 
 data = {'gamma_0': gamma_0, 'beta_0': beta_0, 'gamma_1': gamma_1, 'beta_1': beta_1}
 key_template = 'proc_{}#after_q{}#{}'
 
 lr = 1e-1
-tol = 1e-3
+tol = 1e-4
 
 for n in range(nq):
     
     print('Qubit {} starting on process {}...'.format(n+1, r))
         
     params, Fs = rx_optimization(logpsi, n, beta_1, tol=tol, lr=lr, lookback=10, resample_phi=1, sigma=0.0,
-                                   psi_mcmc_params=(5000,3,300,15), phi_mcmc_params=(5000,3,300,15),
+                                   psi_mcmc_params=(7000,3,800,30), phi_mcmc_params=(7000,3,800,30),
                                    eps=1e-5, verbose=False)
     
     logpsi.set_flat_params(params)
     logpsi.fold_imag_params()
     
-    data[key_template.format(r, n+1, 'C')] = logpsi.C
-    data[key_template.format(r, n+1, 'a')] = logpsi.a
-    data[key_template.format(r, n+1, 'b')] = logpsi.b
-    data[key_template.format(r, n+1, 'W')] = logpsi.W
-    data[key_template.format(r, n+1, 'Fs')] = Fs
+    data[key_template.format(r, n+1, 'C')] = logpsi.C.copy()
+    data[key_template.format(r, n+1, 'a')] = logpsi.a.copy()
+    data[key_template.format(r, n+1, 'b')] = logpsi.b.copy()
+    data[key_template.format(r, n+1, 'W')] = logpsi.W.copy()
+    data[key_template.format(r, n+1, 'Fs')] = Fs.copy()
         
     print('\nQubit {} done on process {}. Final fidelity estimate: {:05.4f}'.format(n+1, r, Fs[-1]))
 
 #### WRITING FILES ####
 
-save_folder = os.path.join(os.getcwd(), 'output_data_7pi16')
+save_folder = os.path.join(os.getcwd(), 'output_data_7pi16_2')
 
 if not os.path.exists(save_folder):
     os.mkdir(save_folder)
