60ce09ce2fde981b5f5cccbd6723e6740977e9b1
Matija Medvidović <matija.medvidovic@gmail.com>
2020-06-27 00:25:18 +0200

    Hyperparameter changes

--- cluster/qaoa_54_p1_sweep.py
+++ cluster/qaoa_54_p1_sweep.py
@@ -137,7 +137,7 @@ for i, j in G.edges():
 
 logpsi.fold_imag_params()
 
-alpha = 2.2
+alpha = 2
 logpsi.add_hidden_units(num=floor(alpha*logpsi.nv - logpsi.nh))
 logpsi.mask[:] = True
 
@@ -149,7 +149,7 @@ for n in range(nq):
     print('Qubit {} starting on process {}...'.format(n+1, r))
         
     params, Fs = rx_optimization(logpsi, n, beta_opt, tol=1e-3, lr=1e-1, lookback=5, resample_phi=5, sigma=0.0,
-                                   psi_mcmc_params=(1500,4,500,54), phi_mcmc_params=(1500,4,500,54),
+                                   psi_mcmc_params=(2500,4,1000,54), phi_mcmc_params=(2500,4,1000,54),
                                    eps=1e-5, verbose=False)
     
     logpsi.params = params
