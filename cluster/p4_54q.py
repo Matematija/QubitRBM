68a03c87781500accd699fa4f6eec305d09b4327
Matija Medvidović <matija.medvidovic@gmail.com>
2020-07-17 15:55:58 +0200

    Trying to get away with parameter restrictions

--- cluster/p4_54q.py
+++ cluster/p4_54q.py
@@ -12,8 +12,8 @@ p = 4
 k = 3
 nq = 54
 
-compression_ps = [3, 4]
-target_nh = 3*nq
+# compression_ps = [3, 4]
+# target_nh = 3*nq
 
 data = {}
 
@@ -54,26 +54,28 @@ for i in range(p):
     for u, v in G.edges(): 
         logpsi.RZZ(u, v, phi=2*gammas[i])
 
+    # logpsi.mask[:] = True
+
     data['params_after_p{}_UC'.format(i+1)] = logpsi.params
 
-    if i+1 in compression_ps:
+    # if i+1 in compression_ps:
 
-        print('Starting compression at p={}...'.format(i+1))
+    #     print('Starting compression at p={}...'.format(i+1))
 
-        a = logpsi.a.copy()
-        b = logpsi.b.copy()[:target_nh]
-        W = logpsi.W.copy()[:,:target_nh]
+    #     a = logpsi.a.copy()
+    #     b = logpsi.b.copy()[:target_nh]
+    #     W = logpsi.W.copy()[:,:target_nh]
 
-        init = np.concatenate(a, b, W.reshape(-1))
+    #     init = np.concatenate(a, b, W.reshape(-1))
 
-        logpsi, Fs = compress_rbm(logpsi, target_hidden_num=target_nh, init=init, tol=1e-3, lookback=5, max_iters=100,
-                                    psi_mcmc_params=(2000,4,500,54), phi_mcmc_params=(2000,4,500,54), sigma=0.0,
-                                    resample_phi=5, lr=1e-1, eps=1e-4, verbose=True)
+    #     logpsi, Fs = compress_rbm(logpsi, target_hidden_num=target_nh, init=init, tol=1e-3, lookback=5, max_iters=500,
+    #                                 psi_mcmc_params=(2000,4,500,54), phi_mcmc_params=(2000,4,500,54), sigma=0.0,
+    #                                 resample_phi=5, lr=1e-1, eps=1e-4, verbose=True)
 
-        logpsi.fold_imag_params()
+    #     logpsi.fold_imag_params()
 
-        data['params_after_p{}_compression'.format(i+1)] = logpsi.params
-        np.savez('p4_54q.npz', **data)
+    #     data['params_after_p{}_compression'.format(i+1)] = logpsi.params
+    #     np.savez('p4_54q.npz', **data)
 
     print('Starting optimizations at p={}...'.format(i+1))
 
