98f1075877acf114a26714cd9d68134a74b1e87c
Matija Medvidović <matija.medvidovic@gmail.com>
2020-07-20 15:19:21 +0200

    Another p=4 run

--- cluster/p4_54q.py
+++ cluster/p4_54q.py
@@ -1,21 +1,21 @@
 import numpy as np
 import sys, os
 import networkx as nx
+from collections import OrderedDict
 
 sys.path.append(os.path.abspath('..'))
 
-from QubitRBM.optimize import rx_optimization, compress_rbm
+# from QubitRBM.optimize import rx_optimization, compress_rbm
 from QubitRBM.rbm import RBM
+from QubitRBM.optimize import Optimizer
 from QubitRBM.qaoa import QAOA
 
 p = 4
 k = 3
 nq = 54
+mcmc_params = OrderedDict(zip(['n_steps', 'n_chains', 'warmup', 'step'], [2000, 4, 500, nq]))
 
-# compression_ps = [3, 4]
-# target_nh = 3*nq
-
-data = {}
+data = OrderedDict()
 
 G20 = nx.random_regular_graph(k, 20)
 while not nx.is_connected(G20):
@@ -51,31 +51,27 @@ for i in range(p):
 
     print('Applying UC at p={}...'.format(i+1))
 
-    for u, v in G.edges(): 
-        logpsi.RZZ(u, v, phi=2*gammas[i])
-
-    # logpsi.mask[:] = True
+    logpsi.UC(G, gammas[i], mask=False)
+    logpsi.mask[:] = True
 
     data['params_after_p{}_UC'.format(i+1)] = logpsi.params
 
-    # if i+1 in compression_ps:
+    optim = Optimizer(logpsi, **mcmc_params)
 
-    #     print('Starting compression at p={}...'.format(i+1))
+    if i > 0:
 
-    #     a = logpsi.a.copy()
-    #     b = logpsi.b.copy()[:target_nh]
-    #     W = logpsi.W.copy()[:,:target_nh]
+        print('Starting compression at p={}...'.format(i+1))
 
-    #     init = np.concatenate(a, b, W.reshape(-1))
+        aux = RBM(n_visible=nq)
+        aux.UC(G, gammas[:i+1].sum(), mask=False)
+        init = aux.params
 
-    #     logpsi, Fs = compress_rbm(logpsi, target_hidden_num=target_nh, init=init, tol=1e-3, lookback=5, max_iters=500,
-    #                                 psi_mcmc_params=(2000,4,500,54), phi_mcmc_params=(2000,4,500,54), sigma=0.0,
-    #                                 resample_phi=5, lr=1e-1, eps=1e-4, verbose=True)
+        logpsi, Fs = optim.sr_compress(init=init, tol=1e-3, lookback=5, max_iters=1000, resample_phi=5, lr=1e-1, eps=1e-4, verbose=True)
 
-    #     logpsi.fold_imag_params()
+        logpsi.fold_imag_params()
 
-    #     data['params_after_p{}_compression'.format(i+1)] = logpsi.params
-    #     np.savez('p4_54q.npz', **data)
+        data['params_after_p{}_compression'.format(i+1)] = logpsi.params
+        np.savez('p4_54q.npz', **data)
 
     print('Starting optimizations at p={}...'.format(i+1))
 
@@ -83,8 +79,7 @@ for i in range(p):
         
         print('Qubit {} starting at p={}...'.format(n+1, i+1))
             
-        params, Fs = rx_optimization(rbm=logpsi, n=n, beta=betas[i], tol=1e-3, lr=1e-1, lookback=5, resample_phi=5, sigma=0.0,
-                                    psi_mcmc_params=(2000,4,500,54), phi_mcmc_params=(2000,4,500,54), eps=1e-4, verbose=True)
+        params, Fs = optim.sr_rx(n, betas[i], tol=1e-3, lookback=3, max_iters=1000, resample_phi=5, lr=1e-1, eps=1e-4, verbose=True)
         
         logpsi.params = params
         logpsi.fold_imag_params()
