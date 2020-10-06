6e63c76e1232a24c7fed4f54d49b32962d9c3d51
Matija Medvidovic <mmedvidovic@rusty1.cm.cluster>
2020-10-06 16:36:03 -0400

    Renamed dir for compatibility

---
+++ qubitrbm/utils.py
@@ -0,0 +1,51 @@
+import numpy as np
+from scipy.special import logsumexp
+from numba import njit
+
+def _bit(x, k, n):
+    """
+    Returns the k-th binary digit of number x, padded with zeros to length n.
+    """
+    return int(bin(x)[2:].rjust(n,'0')[k])
+
+def hilbert_iter(n_qubits):
+    for n in range(2**n_qubits):
+        yield np.fromiter(map(int, np.binary_repr(n, width=n_qubits)), dtype=np.bool, count=n_qubits)
+
+@njit
+def log1pexp(z):   
+    m = np.max(z.real)
+    return m + np.log(np.exp(-m) + np.exp(z-m))
+
+@njit
+def logcosh(z):
+    return -np.log(2) + z + log1pexp(-2*z)
+
+@njit
+def sigmoid(z):
+    return 0.5*(1 + np.tanh(z/2))
+
+@njit
+def logaddexp(x, y, b=(1.0, 1.0)):
+
+    mx = np.max(x.real)
+    my = np.max(y.real)
+    m = mx if mx > my else my
+
+    return m + np.log(b[0]*np.exp(x-m) + b[1]*np.exp(y-m))
+
+def fold_imag(z):
+    return np.real(z) + 1j*((np.imag(z) + np.pi)%(2*np.pi) - np.pi)
+
+def exact_fidelity(psi, phi):
+    return np.abs(np.vdot(psi, phi))**2
+
+def mcmc_fidelity(psipsi, psiphi, phipsi, phiphi):
+    term_1 = logsumexp(phipsi - psipsi, b=1/phipsi.shape[0])
+    term_2 = logsumexp(psiphi - phiphi, b=1/psiphi.shape[0])
+
+    return np.exp(term_1 + term_2).real
+
+def bootstrap_cost_error(vals):
+    n = vals.shape[0]
+    return np.random.choice(vals, size=(n,n), replace=True).mean(axis=1).std()
