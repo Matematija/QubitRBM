7ddd944772f142f20e68856b10f5c98e31fe825c
Matija Medvidović <matija.medvidovic@gmail.com>
2021-02-21 00:27:22 +0100

    Added docs to minor code consistency changes

--- qubitrbm/optimize.py
+++ qubitrbm/optimize.py
@@ -19,6 +19,28 @@ import qubitrbm.utils as utils
 class Optimizer:
     
     def __init__(self, machine, **mcmc_params):
+
+        """
+        Takes a RBM to perform different optimizations on: stochastic optimization to apply Pauli X roation gates or compressing the number of hidden units.
+
+        Constructor arguments:
+
+        machine: RBM
+            The RBM instance to make a copy of and use for optimization.
+
+        n_steps: Int
+            The number of samples to obtain.
+        
+        n_chains: Int (default=1)
+            The number of independent Markov chains to run.
+            
+        warmup: Int (default=0)
+            The number of consecutive samples to discard in each Markov chain to facilitate equilibriation,
+            
+        step: Int (default=1)
+            The number of MCMC steps to take in between samples. (step=1 means record each sample, step=2 means record every other...)
+        """
+
         self.machine = machine
         self.mcmc_params = mcmc_params
         
@@ -35,6 +57,21 @@ class Optimizer:
         
     @property
     def mcmc_params(self):
+        """
+        Returns an OrderedDict containing all MCMC parameters that are forwarded to self.machine whenever sampling is required. Keys:
+
+        n_steps: Int
+            The number of samples to obtain.
+        
+        n_chains: Int (default=1)
+            The number of independent Markov chains to run.
+            
+        warmup: Int (default=0)
+            The number of consecutive samples to discard in each Markov chain to facilitate equilibriation,
+            
+        step: Int (default=1)
+            The number of MCMC steps to take in between samples. (step=1 means record each sample, step=2 means record every other...)
+        """
         return self.__mcmc_params
     
     @mcmc_params.setter
@@ -90,6 +127,16 @@ class Optimizer:
 
     @staticmethod
     def maxcut_cost(graph, configs):
+        """
+        Calculates the MaxCut cost associated with a batch of bitstrings.
+
+        graph: networkx.Graph
+            The MaxCut graph.
+
+        configs: 1D or 2D numpy.array of shape [self .nv] or [batch_size, self.nv]
+            Input bitstrings for cost calculation.
+        """
+
         B = np.atleast_2d(configs)
         z = (-1)**B[:, graph.edges()]
         return z.prod(axis=-1).sum(axis=-1)
@@ -101,7 +148,49 @@ class Optimizer:
         return term_1 - term_2
     
     def sr_rx(self, n, beta, tol=1e-3, lookback=5, max_iters=1000, resample_phi=None,
-              lr=1e-1, lr_tau=None, lr_min=0.0, eps=1e-4, verbose=False):
+        lr=1e-1, lr_tau=None, lr_min=0.0, eps=1e-4, verbose=False):
+        """
+        Performs the stochastic optimization associated with applying the Pauli X rotation gate to qubit n.
+
+        n: Int
+            Determines the qubit to apply the RX gate on.
+
+        beta: float
+            The angle of the RX gate. In other words, it's the \beta in exp(-i*\beta X_n) |RBM>.
+
+        tol: float
+            How close to fidelity=1 is good enough - optimization stops when the averaged fidelity estimate exceeds 1 - tol.
+
+        lookback: Int
+            The number of optimization steps to look back and take fidelity values from. Since fidelity estimates are noisy, the stopping criterion for optimization is:
+                a) Look at the last `lookback` fidelity estimate values.
+                b) If their average is 1-tol, stop the optimization. Else, continue.
+
+        max_iters: Int
+            The maximum number of iterations allowed.
+
+        resample_phi: Int
+            How many optimization steps is the optimizer allowed to go before it has to draw new samples from the target state. More frequent resampling yields more accurate results but incurs a computational cost.
+
+        lr: float
+            The learning rate (imaginary time increment) for the stochastic reconfiguration optimization algorithm.
+
+        lr_tau: float or None
+            The learning rate exponential decay rate, according to the law lr(t) = lr(0)*exp(-t/lr_tau) at optimizer step t. If lr_tau is not none, then `lr` is interpreted as the learning rate initial value, lr(0).
+
+        lr_min: float [Only relevant when lr_tau is not None]
+            When to cap the learning rate exponential decay. The actual learning rate used at step t is the maximum of lr(0)*exp(-t/lr_tau) and lr_min.
+
+        eps: float
+            The regularizer to use for the S-matrix (complex Fisher information matrix) used with the stochastic reconfiguration algorithm. To avoid ill-conditioned linear systems stemming from noisy matrix estimates, S + eps*I (I = identity matrix) is used instead of S itself.
+
+        verbose: bool
+            Whether to print out optimizer progress report every 10 seconds.
+
+        Returns: A tuple of two numpy.arrays
+            1) New parameters of shape [self.machine.n_free_par].
+            2) Quantum fidelities as a function of optimization step (1D array of shape [# of optimizer steps taken])
+        """
         
         nv, nh = self.machine.nv, self.machine.nh
 
@@ -161,6 +250,46 @@ class Optimizer:
     
     def sr_compress(self, init, tol=1e-3, lookback=5, max_iters=1000, resample_phi=None,
                     lr=1e-1, lr_tau=None, lr_min=0.0, eps=1e-4, verbose=False):
+
+        """
+        Performs the stochastic optimization associated with reducing the number of hidden units in an RBM ansatz (compression).
+
+        init: numpy.array
+            Initial parameters of the smaller RBM. The number of visible units is assumed to be equal to self.machine.nv so the number of hidden units can calculated from the length of the given `init` array.
+
+        tol: float
+            How close to fidelity=1 is good enough - optimization stops when the averaged fidelity estimate exceeds 1 - tol.
+
+        lookback: Int
+            The number of optimization steps to look back and take fidelity values from. Since fidelity estimates are noisy, the stopping criterion for optimization is:
+                a) Look at the last `lookback` fidelity estimate values.
+                b) If their average is 1-tol, stop the optimization. Else, continue.
+
+        max_iters: Int
+            The maximum number of iterations allowed.
+
+        resample_phi: Int
+            How many optimization steps is the optimizer allowed to go before it has to draw new samples from the target state. More frequent resampling yields more accurate results but incurs a computational cost.
+
+        lr: float
+            The learning rate (imaginary time increment) for the stochastic reconfiguration optimization algorithm.
+
+        lr_tau: float or None
+            The learning rate exponential decay rate, according to the law lr(t) = lr(0)*exp(-t/lr_tau) at optimizer step t. If lr_tau is not none, then `lr` is interpreted as the learning rate initial value, lr(0).
+
+        lr_min: float [Only relevant when lr_tau is not None]
+            When to cap the learning rate exponential decay. The actual learning rate used at step t is the maximum of lr(0)*exp(-t/lr_tau) and lr_min.
+
+        eps: float
+            The regularizer to use for the S-matrix (complex Fisher information matrix) used with the stochastic reconfiguration algorithm. To avoid ill-conditioned linear systems stemming from noisy matrix estimates, S + eps*I (I = identity matrix) is used instead of S itself.
+
+        verbose: bool
+            Whether to print out optimizer progress report every 10 seconds.
+
+        Returns: A tuple of two numpy.arrays
+            1) New parameters of shape init.shape.
+            2) Quantum fidelities as a function of optimization step (1D array of shape [# of optimizer steps taken])
+        """
         
         nv = self.machine.nv
         target_nh = int((len(init) - nv)/(nv + 1))
@@ -215,91 +344,125 @@ class Optimizer:
                 print(self.__printout_template.format(t, F, lr_, diff_mean_F))
                 clock = time()
 
-        return logpsi, np.asarray(history)
+        return logpsi.params, np.asarray(history)
     
-    def adam_compress(self, rbm, init, lr=1e-3, tol=1e-3, lookback=20, resample_phi=None, max_iters=1000,
-                     betas=(0.9, 0.999), eps=1e-6, verbose=False):
+    # def adam_compress(self, rbm, init, lr=1e-3, tol=1e-3, lookback=20, resample_phi=None, max_iters=1000,
+    #                  betas=(0.9, 0.999), eps=1e-6, verbose=False):
     
-        nv = self.machine.nv
-        target_nh = int((len(init) - nv)/(nv + 1))
+    #     nv = self.machine.nv
+    #     target_nh = int((len(init) - nv)/(nv + 1))
 
-        logpsi = RBM(n_visible=nv, n_hidden=target_nh)
-        logpsi.params = init
+    #     logpsi = RBM(n_visible=nv, n_hidden=target_nh)
+    #     logpsi.params = init
 
-        params = init.copy()
-        phi_samples = self.machine.get_samples(**self.mcmc_params)
-        phiphi = self.machine(phi_samples)
+    #     params = init.copy()
+    #     phi_samples = self.machine.get_samples(**self.mcmc_params)
+    #     phiphi = self.machine(phi_samples)
 
-        history = []
-        F, F_mean_new, F_mean_old = 0.0, 0.0, 0.0
-        diff_mean_F = 2*tol
-        lr_ = lr
-        clock = time()
-        t = 0
+    #     history = []
+    #     F, F_mean_new, F_mean_old = 0.0, 0.0, 0.0
+    #     diff_mean_F = 2*tol
+    #     lr_ = lr
+    #     clock = time()
+    #     t = 0
 
-        beta1, beta2 = betas
-        mr = np.zeros(len(params))
-        mi = np.zeros(len(params))
-        vr = np.zeros(len(params))
-        vi = np.zeros(len(params))
+    #     beta1, beta2 = betas
+    #     mr = np.zeros(len(params))
+    #     mi = np.zeros(len(params))
+    #     vr = np.zeros(len(params))
+    #     vi = np.zeros(len(params))
 
-        while (diff_mean_F > tol or t < 2*lookback + 1) and F_mean_new < 0.999 and t < max_iters:
+    #     while (diff_mean_F > tol or t < 2*lookback + 1) and F_mean_new < 0.999 and t < max_iters:
 
-            t += 1
+    #         t += 1
 
-            psi_samples = logpsi.get_samples(**self.mcmc_params)
+    #         psi_samples = logpsi.get_samples(**self.mcmc_params)
             
-            psipsi = logpsi(psi_samples)
-            phipsi = self.machine(psi_samples)
+    #         psipsi = logpsi(psi_samples)
+    #         phipsi = self.machine(psi_samples)
 
-            O = logpsi.grad_log(psi_samples)
-            grad_logF = self.__grad_log_F(O, psipsi, phipsi)
+    #         O = logpsi.grad_log(psi_samples)
+    #         grad_logF = self.__grad_log_F(O, psipsi, phipsi)
 
-            ########## Adam update ##########
+    #         ########## Adam update ##########
 
-            gr, gi = np.real(grad_logF), np.imag(grad_logF)
+    #         gr, gi = np.real(grad_logF), np.imag(grad_logF)
 
-            mr = beta1*mr + (1-beta1)*gr
-            mi = beta1*mi + (1-beta1)*gi
-            vr = beta2*vr + (1-beta2)*(gr**2)
-            vi = beta2*vi + (1-beta2)*(gi**2)
+    #         mr = beta1*mr + (1-beta1)*gr
+    #         mi = beta1*mi + (1-beta1)*gi
+    #         vr = beta2*vr + (1-beta2)*(gr**2)
+    #         vi = beta2*vi + (1-beta2)*(gi**2)
 
-            mr_hat = mr/(1 - beta1**t)
-            mi_hat = mi/(1 - beta1**t)
-            vr_hat = vr/(1 - beta2**t)
-            vi_hat = vi/(1 - beta2**t)
+    #         mr_hat = mr/(1 - beta1**t)
+    #         mi_hat = mi/(1 - beta1**t)
+    #         vr_hat = vr/(1 - beta2**t)
+    #         vi_hat = vi/(1 - beta2**t)
 
-            d_params = mr_hat/(np.sqrt(vr_hat) + eps) + 1j*(mi_hat/(np.sqrt(vi_hat) + eps))
+    #         d_params = mr_hat/(np.sqrt(vr_hat) + eps) + 1j*(mi_hat/(np.sqrt(vi_hat) + eps))
 
-            ################################
+    #         ################################
 
-            params -= lr*d_params
-            logpsi.params = params
+    #         params -= lr*d_params
+    #         logpsi.params = params
             
             
-            psiphi = logpsi(phi_samples)
-            F = utils.mcmc_fidelity(psipsi, psiphi, phipsi, phiphi)
-            history.append(F)
+    #         psiphi = logpsi(phi_samples)
+    #         F = utils.mcmc_fidelity(psipsi, psiphi, phipsi, phiphi)
+    #         history.append(F)
 
-            if t > 2*lookback:
-                F_mean_old = sum(history[-2*lookback:-lookback])/lookback
-                F_mean_new = sum(history[-lookback:])/lookback
+    #         if t > 2*lookback:
+    #             F_mean_old = sum(history[-2*lookback:-lookback])/lookback
+    #             F_mean_new = sum(history[-lookback:])/lookback
                 
-            diff_mean_F = np.abs(F_mean_new - F_mean_old)
+    #         diff_mean_F = np.abs(F_mean_new - F_mean_old)
 
-            if resample_phi is not None:
-                if t%resample_phi == 0:
-                    phi_samples = rbm.get_samples(**self.mcmc_params)
-                    phiphi = self.machine(phi_samples)
+    #         if resample_phi is not None:
+    #             if t%resample_phi == 0:
+    #                 phi_samples = rbm.get_samples(**self.mcmc_params)
+    #                 phiphi = self.machine(phi_samples)
 
-            if time() - clock > 20 and verbose:
-                diff_mean_F = np.abs(F_mean_new - F_mean_old)
-                print(self.__printout_template.format(t, F, lr, diff_mean_F))
-                clock = time()
+    #         if time() - clock > 20 and verbose:
+    #             diff_mean_F = np.abs(F_mean_new - F_mean_old)
+    #             print(self.__printout_template.format(t, F, lr, diff_mean_F))
+    #             clock = time()
 
-        return logpsi, np.asarray(history)
+    #     return logpsi.params, np.asarray(history)
 
     def direct_sr(self, graph, tol=5e-2, lookback=5, max_iters=1000, lr=5e-2, eps=1e-4, verbose=False):
+
+        """
+        Performs the stochastic optimization associated with fitting the RBM to the actual MaxCut solution, without going through any sort of QAOA.
+
+        graph: networkx.Graph
+            The underlying MaxCut graph.
+
+        tol: float
+            How close to fidelity=1 is good enough - optimization stops when the averaged fidelity estimate exceeds 1 - tol.
+
+        lookback: Int
+            The number of optimization steps to look back and take fidelity values from. Since fidelity estimates are noisy, the stopping criterion for optimization is:
+                a) Look at the last `lookback` fidelity estimate values.
+                b) If their average is 1-tol, stop the optimization. Else, continue.
+
+        max_iters: Int
+            The maximum number of iterations allowed.
+
+        resample_phi: Int
+            How many optimization steps is the optimizer allowed to go before it has to draw new samples from the target state. More frequent resampling yields more accurate results but incurs a computational cost.
+
+        lr: float
+            The learning rate (imaginary time increment) for the stochastic reconfiguration optimization algorithm.
+
+        eps: float
+            The regularizer to use for the S-matrix (complex Fisher information matrix) used with the stochastic reconfiguration algorithm. To avoid ill-conditioned linear systems stemming from noisy matrix estimates, S + eps*I (I = identity matrix) is used instead of S itself.
+
+        verbose: bool
+            Whether to print out optimizer progress report every 10 seconds.
+
+        Returns: A tuple of two numpy.arrays
+            1) Optimal parameters of shape init.shape.
+            2) MaxCut costs as a function of optimization step (1D array of shape [# of optimizer steps taken])
+        """
 
         nv, nh = self.machine.nv, self.machine.nh
 
--- qubitrbm/qaoa.py
+++ qubitrbm/qaoa.py
@@ -15,6 +15,18 @@ import qubitrbm.utils as utils
 class QAOA:
     
     def __init__(self, graph, p=1):
+
+        """
+        A class that wraps around Cirq simulators to extract interesting information about smaller QAOA instances.
+
+        Constructor arguments:
+
+        graph: networkx.Graph
+            The underlying QAOA graph
+
+        p: Int
+            The QAOA depth.
+        """
         
         self.n_qubits = len(graph.nodes)
         self.qubits = cirq.LineQubit.range(self.n_qubits)
@@ -48,10 +60,16 @@ class QAOA:
 
     @property
     def circuit(self):
+        """
+        The underlying Cirq.Circuit object representing the specific QAOA circuit.
+        """
         return self.__circuit
 
     @property
     def graph(self):
+        """
+        The underlying networkx.Graph object representing the specific QAOA graph.
+        """
         return self.__graph
 
     @graph.setter
@@ -70,6 +88,9 @@ class QAOA:
 
     @property
     def p(self):
+        """
+        The QAOA depth p.
+        """
         return self.__p
 
     @p.setter
@@ -84,20 +105,66 @@ class QAOA:
         return cirq.ParamResolver({**gamma_dict, **beta_dict})
 
     def sample(self, gamma, beta, n_samples):
+        """
+        Get samples from the underlying Cirq quantum cirquit simulator.
+
+        gamma: float (if self.p==1) or numpy.array of shape [self.p]
+            Values of QAOA \gamma parameters.
+
+        beta: float (if self.p==1) or numpy.array of shape [self.p]
+            Values of QAOA \beta parameters.
+
+        n_samples: Int
+            The number of samples required.
+
+        Returns: A numpy.array of bitstrings with shape [n_samples, n_qubits].
+        """
         param_res = self.__get_param_resolver(gamma, beta)
         res = self.sim.sample(program=self.__circuit, params=param_res, repetitions=n_samples)
         return res.drop(labels=[col for col in res.columns if 'gamma' in col or 'beta' in col], axis=1).values
 
     def simulate(self, gamma, beta):
+        """
+        Get the full normalized output state of the QAOA circuit.
+
+        gamma: float (if self.p==1) or numpy.array of shape [self.p]
+            Values of QAOA \gamma parameters.
+
+        beta: float (if self.p==1) or numpy.array of shape [self.p]
+            Values of QAOA \beta parameters.
+
+        Returns: A cirq.StateVectorTrialResult object.
+        (Full array of amplitudes can be accessed with the .final_state_vector method.)
+        """
         param_res = self.__get_param_resolver(gamma, beta)
         return self.sim.simulate(self.__circuit[:-1], param_resolver=param_res, qubit_order=self.qubits)
 
     def cost(self, configs):
+        """
+        Calculate the MaxCut cost for given bitstrings
+
+        configs: numpy.array of shape [batch_size, qubit_number]
+            The batch of bitstrings to calculate MaxCut costs for.
+
+        Returns: A 1D numpy.array of cost values corresponding to rows in `configs`.
+        """
         B = np.atleast_2d(configs)
         z = (-1)**B[:, self.graph.edges()]
         return z.prod(axis=-1).sum(axis=-1)
 
     def exact_cost(self, gamma, beta):
+
+        """
+        Calculate the exact value of the MaxCut cost at given angles if p=1.
+
+        gamma: float or numpy.array of shape [batch_size]
+            Values of QAOA \gamma parameters.
+
+        beta: float or numpy.array of shape [batch_size]
+            Values of QAOA \beta parameters.
+
+        Returns: A float or 1D numpy.array of cost values corresponding to matching entries in `gamma` and `beta`.
+        """
         
         assert self.p == 1, 'The exact formula holds only for p=1!'
 
@@ -119,12 +186,44 @@ class QAOA:
 
     def cost_from_probs(self, probs, hilbert=None):
 
+        """
+        Calculates cost from all bitstring probabilities. A helper function.
+
+        probs: numpy.array
+            An array of all 2**n_qubits probability values that should sum to 1.
+
+        hilbert: 2D numpy.array or None (default=None)
+            A 2D array containing all 2**self.nv bitstrings for the given graph.
+            (If not provided, it will be calculated using qubitrbm.utils.hilbert_iter.)
+
+        """
+
         if hilbert is None:
             hilbert  = np.array(list(utils.hilbert_iter(self.n_qubits))) 
 
         return np.sum(probs*self.cost(hilbert))
 
     def num_cost_from_params(self, gamma, beta, method='exact', **kwargs):
+
+        """
+        Calculate the MaxCut cost using either the exact output wavefunction from the underlying Cirq simulator or estimate the cost using output state samples.
+        
+        gamma: float (if self.p==1) or numpy.array of shape [self.p]
+            Values of QAOA \gamma parameters.
+
+        beta: float (if self.p==1) or numpy.array of shape [self.p]
+            Values of QAOA \beta parameters.
+
+        method: str
+            Either 'exact' or 'sample', choosing between the two methods of calculating the cost.
+
+        hilbert: 2D numpy.array or None (default=None) [Used only if method='exact']
+            A 2D array containing all 2**self.nv bitstrings for the given graph.
+            (If not provided, it will be calculated using qubitrbm.utils.hilbert_iter.)
+
+        n_samples: Int [Used only of method='sample']
+            The number of samples required.
+        """
 
         if method.lower() == 'exact':
             psi = self.simulate(gamma, beta).final_state
@@ -139,6 +238,21 @@ class QAOA:
             raise KeyError('Expected "exact" or "sampling" for method, got {}'.format(method))
 
     def num_grad_cost(self, gamma, beta, dx=1e-5, hilbert=None):
+        """
+        Calculate the finite-difference estimator of the derivative of the MaxCut cost with respect to the QAOA angles.
+
+        gamma: float (if self.p==1) or numpy.array of shape [self.p]
+            Values of QAOA \gamma parameters.
+
+        beta: float (if self.p==1) or numpy.array of shape [self.p]
+            Values of QAOA \beta parameters.
+
+        dx: float
+            The finite difference for angles.
+
+        Returns:
+            A 1d numpy.array of length 2p: first p elements represent gradients w.r.t. QAOA \gamma parameters and the second p elements represent gradients w.r.t. QAOA \beta parameters.
+        """
 
         gs = np.atleast_1d(gamma)
         bs = np.atleast_1d(beta)
@@ -164,6 +278,20 @@ class QAOA:
         return grad
 
     def exact_grad_cost(self, gamma, beta):
+
+        """
+        Efficiently calculate the finite-difference estimator of the derivative of the MaxCut cost with respect to the QAOA angles if p=1.
+
+        gamma: float or numpy.array of shape [batch_size]
+            Values of QAOA \gamma parameters.
+
+        beta: float or numpy.array of shape [batch_size]
+            Values of QAOA \beta parameters.
+
+        Returns:
+            A numpy.array of shape [batch_size,2] containing gradients w.r.t. gamma & beta.
+        
+        """
 
         assert self.p == 1, 'The exact formula holds only for p=1!'
 
@@ -192,6 +320,39 @@ class QAOA:
         return np.array([d_gamma, d_beta], dtype=np.float)
 
     def optimize(self, lr=1e-3, tol=1e-3, init=None, betas=(0.9, 0.999), eps=1e-6, hilbert=None, dx=1e-5, verbose=True):
+
+        """
+        Use the Adam optimizer to find the minimum of the MaxCut cost as a function of QAOA parameters. If QAOA.p is set to 1, then fast exact cost values and fast exact gradients will be used automatically. Otherwise, costs are evaluated through circuit simulation and gradients through finite difference approximatiomns.
+
+        lr: float
+            The Adam learning rate parameter.
+
+        tol: float
+            Sets the tolerance for stopping the optimization - when two consecutive cost values are less than `tol` apart, the optimization loop exits.
+        
+        init: numpy.array or None
+            Initial angles to start the optimization from. The first half is interpreted as gammas and the second as betas.
+
+        betas: tuple of 2 floats
+            Beta parameters for Adam.
+
+        eps: float
+            The \epsilon parameter for Adam.
+
+        hilbert: 2D numpy.array or None (default=None) [Used only if p>1]
+            A 2D array containing all 2**self.nv bitstrings for the given graph.
+            (If not provided, it will be calculated using qubitrbm.utils.hilbert_iter.)
+
+        dx: float [Used only if p>1]
+            The finite difference for gradient estimators.
+
+        verbose: bool
+            Whether to print out optimizer progress report every 10 seconds.
+
+        Returns: A 2-tuple of numpy.arrays
+            1) Optimal angles of shape init.shape.
+            2) MaxCut costs as a function of optimization step (1D array of shape [# of optimizer steps taken])
+        """
 
         beta1, beta2 = betas
         
